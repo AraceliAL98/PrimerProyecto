ee861d1e7108860b2c4fb23b6cb4570f7ed5ab52
Araceli Miyanomori <Araceli Miyanomori@LAPTOP-TV3PHC9Q>
2018-08-13 08:26:35 -0700

    cambios

---
+++ PrimerProyecto/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+    class Persona
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+
+        
+
+        public Persona(int edad, string nombre)
+        {
+            this.Edad = edad;
+
+            this.Nombre = nombre;
+        
+        }
+
+        public Persona()
+
+        {
+            Edad = 0;
+
+           Nombre = "";
+        }
+
+        public void cumplirAno()
+        {
+            Edad++;
+        }
+    }
+}
